74701955ead1a914daa8fd665093bed966d99a8e
Madeline Topf <48026688+topfm@users.noreply.github.com>
2024-10-01 14:54:46 -0500

    Update pangraph.sh
    removing excess commands

--- pangraph.sh
+++ pangraph.sh
@@ -1,8 +1,2 @@
 pangraph build --circular --alpha 0 --beta 0 colonyMorphotype.fa > graph.json
 pangraph export --edge-minimum-length 0 graph.json -p graphExport  -o ./
-#wget https://raw.githubusercontent.com/liampshaw/pangraph-tutorials/main/scripts/prepare-pangraph-gfa.py
-#python prepare-pangraph-gfa.py graphExport.gfa
-
-#wget https://raw.githubusercontent.com/liampshaw/pangraph-tutorials/main/scripts/plot-blocks.R
-#Rscript plot-blocks.R panGraph.blocks.csv $geneBlock graphE
-
