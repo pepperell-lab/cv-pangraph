8d7c6dce05290837f0a795c00582358c5202f820
Madeline Topf <48026688+topfm@users.noreply.github.com>
2024-10-02 16:03:38 -0500

    Update plotPangraph.sh

--- plotPangraph.sh
+++ plotPangraph.sh
@@ -13,10 +13,12 @@ conda install python=3.9 r-base=4.4.1
 conda install -c bioconda bandage
 conda install -c conda-forge -c bioconda mmseqs2=13.45111 mash=2.2.2 mafft=7.475 FastTree=2.1.11
 
+##get prepare-pangraph script
 wget https://raw.githubusercontent.com/liampshaw/pangraph-tutorials/main/scripts/prepare-pangraph-gfa.py
-wget https://raw.githubusercontent.com/liampshaw/pangraph-tutorials/main/scripts/plot-blocks.R
-
 python prepare-pangraph-gfa.py graphExport.gfa
+
+##run Bandage
 Bandage image graphExport.gfa.coloured.gfa graphExport.gfa.png --height 4000 --width 7000 --colour random
 
+##remove installer
 rm Miniconda3-latest-Linux-x86_64.sh
